3cc3c1176b965892f1d1f7e10433e16c3c7ee52d
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-05-24 14:57:51 +0000

    Procfile added

--- builder_test.go
+++ builder_test.go
@@ -12,4 +12,6 @@ import (
 func TestNewBuilder(t *testing.T) {
 	b := NewBuilder(4, 100)
 	if got, want := len(b.data), 101; got != want {
-		t.Fatalf("got: %v\n want: %v\n ", got, wan
+		t.Fatalf("got: %v\n want: %v\n ", got, want)
+	}
+	if got, want := len(b
