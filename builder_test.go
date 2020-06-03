fd452d83e49974ebcd2898ac65e22ebe98d96b50
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-06-03 06:48:42 +0000

    Bump version number to 0.1.1

--- builder_test.go
+++ builder_test.go
@@ -25,4 +25,5 @@ func TestNewBuilder(t *testing.T) {
 func TestAddFeatureData(t *testing.T) {
 	b := NewBuilder(4, 3)
 	b.addFeatureData("newFeature", []string{"one", "two", "three"})
-	expe
+	expectedData := [][]string{
+	
