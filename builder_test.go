83b83cad839b9c6f01db95206f0da497548f1a4d
ficheindex <100076064+ficheindex@users.noreply.github.com>
2020-08-14 11:13:31 +0000

    try this

--- builder_test.go
+++ builder_test.go
@@ -56,4 +56,4 @@ func TestGetFeatureData(t *testing.T) {
 		Name:     "feat1",
 		Endpoint: "/endpoint1/",
 	}
-	// Note that the test fails when there is 
+	// Note that the test fails when there is a greater featureCo
