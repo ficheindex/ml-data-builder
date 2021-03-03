73b16611e618e5780fe9ed41b94c0d6ba96724c2
ficheindex <100076064+ficheindex@users.noreply.github.com>
2021-03-03 08:00:01 +0000

    ARM: 8160/1: drop warning about return_address not using unwind tables

--- builder_test.go
+++ builder_test.go
@@ -189,4 +189,5 @@ func (fhc fakeHttpClient) Do(req http.Request) (*http.Response, error) {
 	return &http.Response{
 		Status:     "200 OK",
 		StatusCode: 200,
-		Body:       ioutil.NopCloser(bytes.NewBufferString(fake
+		Body:       ioutil.NopCloser(bytes.NewBufferString(fakeResponseDump)),
+	}
