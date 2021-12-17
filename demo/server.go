d59884243d548de83bc11cc3907926ac3bf4cce6
ficheindex <100076064+ficheindex@users.noreply.github.com>
2021-12-17 06:35:35 +0000

    fix database

--- demo/server.go
+++ demo/server.go
@@ -57,4 +57,4 @@ func categoryHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/api/items/", itemsHandler)
-	http.HandleFunc("/api/items/pr
+	http.HandleFunc("/api/items/prices", pricesHan
