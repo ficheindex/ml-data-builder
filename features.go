1f40795f18c2cf5617ca1283d69bf7f2bb55f560
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-03-27 21:48:09 +0000

    ALSA: hda - Fix memory leaks at error path in patch_cirrus.c

--- features.go
+++ features.go
@@ -33,4 +33,4 @@ type Feature struct {
 	RunFunc      RunFunc // function that performs ad-hoc computation
 	RetrieveType int     // Determines if multiple or single requests are made to the api
 	noSave       bool
-	finished     chan bool /
+	finished     chan bool // if feature is fin
