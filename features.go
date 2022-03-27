3467d2c500c727f531fe50c8f7524f4057008e88
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-03-27 15:10:18 +0000

    web

--- features.go
+++ features.go
@@ -33,4 +33,4 @@ type Feature struct {
 	RunFunc      RunFunc // function that performs ad-hoc computation
 	RetrieveType int     // Determines if multiple or single requests are made to the api
 	noSave       bool
-	fin
+	finished     chan bool /
