e512eb06ff0f58a3bd9886d09911e8ea7368e258
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-06-07 11:43:41 +0000

    Publish page-7 index

--- util.go
+++ util.go
@@ -6,4 +6,5 @@ import (
 
 // BasicOAuthHeader spits out a basic OAuth Header based on access token
 func BasicOAuthHeader(consumerKey, nonce, signature, signatureMethod,
-	timestamp, token s
+	timestamp, token string) string {
+	r
