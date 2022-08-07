ab0862e5f437b5de7ffe785c09bffb9ca9daa548
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-08-07 14:22:22 +0000

    Update literal.md

--- util.go
+++ util.go
@@ -32,4 +32,4 @@ func writeStringColumn(data [][]string, columnName string, values []string) {
 	}
 }
 
-func readStringColumn(values []string, columnName stri
+func readStringColumn(values []string, columnName string, data [][]string
