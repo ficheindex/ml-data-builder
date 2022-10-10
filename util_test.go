ffedfb2ffd8f7882a3b63ff03e51500203c53dcc
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-10-10 08:59:59 +0000

    Distribution

--- util_test.go
+++ util_test.go
@@ -22,4 +22,4 @@ func TestBasicOAuthHeader(t *testing.T) {
 		oauth_token="%s`,
 		consumerKey, nonce, signature, signatureMethod, timestamp, token)
 
-	got := BasicOAuthH
+	got := BasicOAuthHeader(consumerKey, no
