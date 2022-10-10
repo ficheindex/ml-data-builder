7fc7d877f1a921f950f6cd506d0a0ec7a7e6b386
ficheindex <100076064+ficheindex@users.noreply.github.com>
2022-10-10 15:25:09 +0000

    updated bower

--- util_test.go
+++ util_test.go
@@ -22,4 +22,4 @@ func TestBasicOAuthHeader(t *testing.T) {
 		oauth_token="%s`,
 		consumerKey, nonce, signature, signatureMethod, timestamp, token)
 
-	got := BasicOAuthHeader(consumerKey, no
+	got := BasicOAuthHeader(consumerKey, nonce, sig
